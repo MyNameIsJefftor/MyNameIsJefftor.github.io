44b49d99d9e134c1ed4a54673e1636e58bdfa910
Kurtis McCammon <maxgamer898@gmail.com>
2021-12-07 14:32:32 -0600

    Slides and Videos
    Created Slides and setup the structure to handle multiple sets. Also added LH videos.

--- scripts.js
+++ scripts.js
@@ -20,5 +20,64 @@ function openPage(pageName, element, color = "#36225d"){
 var slideIndex = 1;
 
 function plusSlides(n, targetShow){
+    console.log("plusSlides " + targetShow)
     showSlides(slideIndex += n,targetShow);
+}
+
+function currentSlide(n, targetShow){
+    showSlides(slideIndex = n, targetShow);
+}
+
+function initSlideshows(){
+    var SlideShows = document.getElementsByClassName('SlideshowContainer');
+    console.log(SlideShows.length);
+    for(var i = 0; i < SlideShows.length; i++){
+        var targetShow = SlideShows[i].id.replace("SlideshowContainer","")
+        showSlides(1, targetShow);
+    }
+}
+
+function showSlides(n, targetShow){
+    var i;
+
+    var dirtySlides = document.getElementsByClassName('Slideshow');
+    var cleanSlides = Array();
+    for(i = 0; i<dirtySlides.length;i++)
+    {
+        if(dirtySlides[i].id == targetShow+'Slide'){
+            cleanSlides.push(dirtySlides[i]);
+        }
+    }
+    if(cleanSlides.length == 0){
+        console.log("cleanSlides Error " + targetShow);
+        return;
+    }
+
+    var dirtyDots = document.getElementsByClassName("SlideshowDot")
+    var cleanDots = Array();
+    for(i = 0; i<dirtyDots.length;i++)
+    {
+        if(dirtyDots[i].id == targetShow+'Dot'){
+            cleanDots.push(dirtyDots[i]);
+        }
+    }
+    if(cleanDots.length == 0){
+        console.log("cleanDots Error " + targetShow);
+        return;
+    }
+
+    if(n>cleanSlides.length){
+        slideIndex = 1;
+    }
+    if(n<1){
+        slideIndex = cleanSlides.length;
+    }
+    for(i=0; i < cleanSlides.length; i++){
+        cleanSlides[i].style.display = "none"
+    }
+    for(i=0; i<cleanSlides.length; i++){
+        cleanDots[i].className = cleanDots[i].className.replace(" SlideshowActive", "");
+    }
+    cleanSlides[slideIndex-1].style.display = "block";
+    cleanDots[slideIndex-1].className += " SlideshowActive";
 }
